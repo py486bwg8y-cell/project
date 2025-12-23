8a0aa5450fc48bba6e2adbae7bb724a3b716316d
Shikimo <matsklyarov@icloud.com>
2025-12-23 22:54:59 +0300

    добавленно: возведение в степень автоматическое возведение в дробные числа проверка деления на 0

---  calculator.py
+++  calculator.py
@@ -1,25 +1,40 @@
 print("калькулятор")
  # ввод чисел
-a = float(input("Первое число"))  # float - дробные числа 
-b = float(input("Второе число"))
 # выбор действия
-operation = input("выберите операцию(+ для сложения; - для вычитания; x для умножения; : для деления: ")
-# выбор сложения
-if operation ==  '+' : # if - истинное условие
+operation = input("выберите операцию(+ для сложения; - для вычитания; x для умножения; : для деления;  для возведения в степень:")
+
+if operation == '+':
+    a = float(input("Первое число"))  # float - дробные числа 
+    b = float(input("Второе число"))
     result = a + b
-    print(f"сумма  сложения: {result}")
-# выбор вычитания
+    print(f"сумма  сложения: {result:g}")
+
 elif operation == '-' : # elif - доп условия
+    a = float(input("Первое число"))  # float - дробные числа 
+    b = float(input("Второе число"))
     result = a - b
-    print(f"результат вычитания: {result}")
-# выбор умножения
+    print(f"результат вычитания: {result:g}")
+
 elif operation == '*' :
+    a = float(input("Первое число"))  # float - дробные числа 
+    b = float(input("Второе число"))
     result = a * b
-    print(f" результат умножения: {result}")
-# выбор деления
+    print(f" результат умножения: {result:g}")
+
 elif operation == ':' :
+    a = float(input("Первое число"))  # float - дробные числа 
+    b = float(input("Второе число"))
     result = a / b
-    print(f" результат деления: {result}")
+    if b != 0:
+     print(f" результат деления: {result:g}")
+    else:
+     print("Ошибка")
+
+elif operation == '' :
+    a = float(input("число возмодимое в степень"))  # float - дробные числа 
+    b = float(input("степень"))
+    result = a ** b
+    print(f"результат возведения в степень: {result:g}")
+
 else: 
     print("неверная операция")
-    print("Shikimo")
