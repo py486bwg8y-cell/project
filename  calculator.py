ee1b916856495c9e12ec1bef69e469d431d69e23
Shikimo <_real_email@example.com>
2025-12-28 19:16:53 +0300

    добавлена зациклинность блока кода

---  calculator.py
+++  calculator.py
@@ -1,39 +1,39 @@
 print("калькулятор")
+while True:
+ operation = input("выберите операцию(+ для сложения; - для вычитания; * для умножения; : для деления;  для возведения в степень:")
 
-operation = input("выберите операцию(+ для сложения; - для вычитания; x для умножения; : для деления;  для возведения в степень:")
+ if operation == '+':
+     a = float(input("Первое число"))  
+     b = float(input("Второе число"))
+     result = a + b
+     print(f"сумма  сложения: {result:g}")
 
-if operation == '+':
-    a = float(input("Первое число"))  
-    b = float(input("Второе число"))
-    result = a + b
-    print(f"сумма  сложения: {result:g}")
+ elif operation == '-' : 
+     a = float(input("Первое число")) 
+     b = float(input("Второе число"))
+     result = a - b
+     print(f"результат вычитания: {result:g}")
 
-elif operation == '-' : 
-    a = float(input("Первое число")) 
-    b = float(input("Второе число"))
-    result = a - b
-    print(f"результат вычитания: {result:g}")
+ elif operation == '*' :
+     a = float(input("Первое число"))  
+     b = float(input("Второе число"))
+     result = a * b
+     print(f" результат умножения: {result:g}")
 
-elif operation == '*' :
-    a = float(input("Первое число"))  
-    b = float(input("Второе число"))
-    result = a * b
-    print(f" результат умножения: {result:g}")
+ elif operation == ':' :
+     a = float(input("Первое число"))  
+     b = float(input("Второе число"))
+     result = a / b
+     if b != 0:
+      print(f" результат деления: {result:g}")
+     else:
+      print("Ошибка")
 
-elif operation == ':' :
-    a = float(input("Первое число"))  
-    b = float(input("Второе число"))
-    result = a / b
-    if b != 0:
-     print(f" результат деления: {result:g}")
-    else:
-     print("Ошибка")
-
-elif operation == '' :
+ elif operation == '' :
     a = float(input("число возводимое в степень"))  
     b = float(input("степень"))
     result = a ** b
     print(f"результат возведения в степень: {result:g}")
 
-else: 
+ else: 
     print("неверная операция")
